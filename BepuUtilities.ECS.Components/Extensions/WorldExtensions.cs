1f9487924bde4713923c9c592e2db14c4ef897ce
Justin Britt <britt@uwindsor.ca>
2022-10-09 08:43:24 -0400

    Added extension methods

---
+++ BepuUtilities.ECS.Components/Extensions/WorldExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace BepuUtilities.ECS.Components.Extensions
+{
+    using System.Linq;
+
+    using DefaultEcs;
+
+    using BepuUtilities.ECS.Components.Structs.Memory;
+    using BepuUtilities.Memory;
+
+    public static class WorldExtensions
+    {
+        public static ref BufferPoolComponent GetBufferPoolComponentLastRef(
+            this World world)
+        {
+            return ref world
+                .GetEntities()
+                .With<BufferPoolComponent>()
+                .AsEnumerable()
+                .Where(w => w.IsEnabled() && w.IsAlive)
+                .Last()
+                .Get<BufferPoolComponent>();
+        }
+
+        public static BufferPool GetBufferPoolLast(
+            this World world)
+        {
+            return world
+                .GetEntities()
+                .With<BufferPoolComponent>()
+                .AsEnumerable()
+                .Where(w => w.IsEnabled() && w.IsAlive)
+                .Select(w => w.Get<BufferPoolComponent>().Value)
+                .Last();
+        }
+    }
+}
